df62c0b36fc7c1dfb9b4fbb3079cb82588c30834
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2026-01-21 16:42:54 +0530

    practice

---
+++ conditions.py
@@ -0,0 +1,13 @@
+enviornment = input("Enter your envionment: ")
+print(type(enviornment))
+change_ticket = False
+
+enviornment = enviornment.casefold()
+
+if enviornment == "prod":
+    change_ticket = True
+    print("Please provide a change ticket")
+elif enviornment == "staging":
+    print("Welcome to staging environment")
+else:
+    print("Please login using your credentials")
